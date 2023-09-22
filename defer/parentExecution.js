d2744b87f5cfeb121f9fa7b08470664f5173faf6
Émile Ré <nemile.re@gmail.com>
2023-09-22 12:21:34 +0200

    Update parentFunction

--- defer/parentExecution.js
+++ defer/parentExecution.js
@@ -4,10 +4,8 @@ import helloWorld from "./helloWorld"
 
 const parentFunction = async () => {
   console.log("Parent", process.env.DEFER_TOKEN)
-  await Promise.all([
-    helloWorld(undefined, {metadata: {new: "NEW", new1: "NEW1", overriden: "OVER1"}}),
-    helloWorld(undefined, {metadata: {new3: "NEW3", new4: "NEW4", overriden: "OVER2"}})
-  ])
+  helloWorld(undefined, {metadata: {new: "NEW", new1: "NEW1", overriden: "OVER1"}}),
+  await helloWorld(undefined, {metadata: {new3: "NEW3", new4: "NEW4", overriden: "OVER2"}})
 }
 
 export default defer(parentFunction)
