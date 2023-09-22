4736dacffa32b8ba65fc8d6fb6e8d073b084433e
Émile Ré <nemile.re@gmail.com>
2023-09-22 12:54:41 +0200

    Update helloWorld

--- defer/helloWorld.js
+++ defer/helloWorld.js
@@ -1,9 +1,9 @@
 import { defer } from "@defer/client";
 
-const helloWorld = async () => {
+const helloWorld = async (name) => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      console.log("Hello World");
+      console.log(`Hello ${name}`);
       resolve()
     }, 1000)
   })
--- defer/parentExecution.js
+++ defer/parentExecution.js
@@ -4,8 +4,8 @@ import helloWorld from "./helloWorld"
 
 const parentFunction = async () => {
   console.log("Parent", process.env.DEFER_TOKEN)
-  helloWorld(undefined, {metadata: {new: "NEW", new1: "NEW1", overriden: "OVER1"}}),
-  await helloWorld(undefined, {metadata: {new3: "NEW3", new4: "NEW4", overriden: "OVER2"}})
+  helloWorld("Foo", {metadata: {new: "NEW", new1: "NEW1", overriden: "OVER1"}}),
+  await helloWorld("Bar", {metadata: {new3: "NEW3", new4: "NEW4", overriden: "OVER2"}})
 }
 
 export default defer(parentFunction)
