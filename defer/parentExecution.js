31a5bbca22c4664a6d2afb22b962aec81614e178
Émile Ré <nemile.re@gmail.com>
2023-09-22 13:00:02 +0200

    Fix metadata

--- defer/parentExecution.js
+++ defer/parentExecution.js
@@ -1,11 +1,12 @@
-import "dotenv/config"
-import {defer} from "@defer/client"
+import {addMetadata, defer} from "@defer/client"
 import helloWorld from "./helloWorld"
 
 const parentFunction = async () => {
   console.log("Parent", process.env.DEFER_TOKEN)
-  helloWorld("Foo", {metadata: {new: "NEW", new1: "NEW1", overriden: "OVER1"}}),
-  await helloWorld("Bar", {metadata: {new3: "NEW3", new4: "NEW4", overriden: "OVER2"}})
+  await Promise.all([
+    addMetadata(helloWorld, {new: "NEW", new1: "NEW1", overriden: "OVER1"})(),
+    addMetadata(helloWorld, {new3: "NEW3", new4: "NEW4", overriden: "OVER2"})()
+  ])
 }
 
 export default defer(parentFunction)
